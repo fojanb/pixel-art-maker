f764bd4235cf8ce113ac44985f84ccb8514aa164
Fojan Babaali <fozhanmail@gmail.com>
2021-03-16 22:04:29 -0700

    Update GridMaker.js

--- src/components/GridMaker/GridMaker.js
+++ src/components/GridMaker/GridMaker.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
+import CreateGrid from "../CreateGrid/CreateGrid";
+import ClearGrid from "../ClearGrid/ClearGrid";
 import "./GridMaker.css";
+
 class GridMaker extends Component {
   state = {};
   render() {
     return (
-      <div className="container">
+      <div className="main">
         <button type="reset">Reset</button>
         <div className="row-column">
           <h3>Row</h3>
           <button>-</button>
           <button>+</button>
+          <span class="w3-badge w3-green">0</span>
         </div>
         <div className="row-column">
           <h3>Column</h3>
           <button>-</button>
           <button>+</button>
+          <span class="w3-badge w3-green">0</span>
+        </div>
+        <div className="create-clear">
+          <CreateGrid />
+          <ClearGrid />
         </div>
       </div>
     );
