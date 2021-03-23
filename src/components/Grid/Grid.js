d1e460db5a392cd94cd04f7d3fa54ab667a3df30
Fojan Babaali <fozhanmail@gmail.com>
2021-03-23 13:51:03 -0700

    Create Grid.js

---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const StyleDiv = styled.div`
+color : red;
+`;
+class Grid extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <StyleDiv>hi</StyleDiv>
+         );
+    }
+}
+ 
+export default Grid;
