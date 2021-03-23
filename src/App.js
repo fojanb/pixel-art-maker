5a4fd44bef5fc5affc77084f1d5ef889ee240ab7
Fojan Babaali <fozhanmail@gmail.com>
2021-03-22 21:45:24 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,12 +3,25 @@ import GridMaker from "./components/GridMaker/GridMaker"
 import './App.css';
 
 class App extends Component {
-  state = {  }
+  state = { 
+    incRow : 0,
+    decRow : 0,
+    incCol : 0,
+    decCol : 0 
+   }
+//-----------------Event Handlers-------------
+incrementRowHandler = () => {
+  let newIncRow = this.state.incRow;
+  newIncRow++;
+  this.setState({incRow : newIncRow});
+
+}
+// -------------------------------------------
   render() { 
     return (  
       <div className="App">
         <h1>Pixel Art Maker</h1>
-        <GridMaker/>
+        <GridMaker incrementRow={this.incrementRowHandler} setIncBadge ={this.state.incRow} />
       </div>
     );
   }
