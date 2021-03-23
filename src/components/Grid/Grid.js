5fdc25f12eebe931df52779f1123ca0ee5a4ba8e
Fojan Babaali <fozhanmail@gmail.com>
2021-03-23 15:41:16 -0700

    Update Grid.js

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,24 +1,25 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import "./Grid.css";
 // -----------------------------------------------------*
+// StyledDiv is grid container
 const StyleDiv = styled.div`
   // regular CSS goes here :
   //   Dynamic Grid
   display: grid;
-  grid-template-columns: repeat(2, 70px);
-  grid-template-rows: repeat(2, 70px);
+  grid-template-columns: repeat(${(props) => props.numberOfColumns}, 20px);
+  grid-template-rows: repeat(${(props) => props.numberOfRows}, 20px);
   margin: 0 auto;
-  background-color: red;
-  border: 1px solid black;
-  margin-top : 20px;
+  justify-content: center;
+  align-item: center;
 `;
 class Grid extends Component {
   state = {};
   render() {
     return (
       <StyleDiv
-        numberOfcolumns={this.props.columns}
-        numberOfrows={this.props.rows}
+        numberOfColumns={this.props.columns}
+        numberOfRows={this.props.rows}
       ></StyleDiv>
     );
   }
