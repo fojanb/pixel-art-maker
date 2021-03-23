355b5b6d5b145e1839d8f562ecc39eb95bf2750e
Fojan Babaali <fozhanmail@gmail.com>
2021-03-22 22:55:20 -0700

    Update CreateGrid.js

--- src/components/CreateGrid/CreateGrid.js
+++ src/components/CreateGrid/CreateGrid.js
@@ -1,9 +1,14 @@
-import React from 'react';
-const CreateGrid = () => {
-    return(
-        <div>
-            <button>Create</button>
-        </div>
-    )
+import React, { Component } from "react";
+
+class CreateGrid extends Component {
+  render() {
+    return (
+      <div>
+        <button onClick={this.props.toggleShowGrid}>Create</button>
+        <span></span>
+      </div>
+    );
+  }
 }
+
 export default CreateGrid;
