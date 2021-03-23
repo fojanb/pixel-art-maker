9676d9d8fa132b7a0bc8264a41445850a57b92ba
Fojan Babaali <fozhanmail@gmail.com>
2021-03-22 22:17:35 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,63 @@
-import React, { Component } from 'react';
-import GridMaker from "./components/GridMaker/GridMaker"
-import './App.css';
+import React, { Component } from "react";
+import GridMaker from "./components/GridMaker/GridMaker";
+import "./App.css";
 
 class App extends Component {
-  state = { 
-    incRow : 0,
-    decRow : 0,
-    incCol : 0,
-    decCol : 0 
-   }
-//-----------------Event Handlers-------------
-incrementRowHandler = () => {
-  let newIncRow = this.state.incRow;
-  newIncRow++;
-  this.setState({incRow : newIncRow});
+  state = {
+    Row: 0,
+    Col: 0,
+  };
+  //-----------------Event Handlers (start)-------------*
+   // -----Row------*
+  incrementRowHandler = () => {
+    let newRow = this.state.Row;
+    newRow++;
+    this.setState({ Row: newRow });
+  };
+  decrementRowHandler = () => {
+    let newRow = this.state.Row;
 
-}
-// -------------------------------------------
-  render() { 
-    return (  
+    if (newRow == 0) {
+      this.setState({ Row: newRow });
+    } else if (newRow > 0) {
+      newRow = newRow - 1;
+    }
+    this.setState({ Row: newRow });
+  };
+
+   // -----Column------*
+  incrementColHandler = () => {
+    let newCol = this.state.Col;
+    newCol++;
+    this.setState({ Col: newCol });
+  };
+  decrementColHandler = () => {
+    let newCol = this.state.Col;
+
+    if (newCol == 0) {
+      this.setState({ Col: newCol });
+    } else if (newCol > 0) {
+      newCol = newCol - 1;
+    }
+    this.setState({ Col: newCol });
+  };
+
+  //-----------------Event Handlers (end)-------------*
+  render() {
+    return (
       <div className="App">
         <h1>Pixel Art Maker</h1>
-        <GridMaker incrementRow={this.incrementRowHandler} setIncBadge ={this.state.incRow} />
+        <GridMaker
+          incrementRow={this.incrementRowHandler}
+          setRowBadge={this.state.Row}
+          decrementRow={this.decrementRowHandler}
+          incrementCol={this.incrementColHandler}
+          setColBadge={this.state.Col}
+          decrementCol={this.decrementColHandler}
+        />
       </div>
     );
   }
 }
- 
+
 export default App;
