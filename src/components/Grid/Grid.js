11ee33be76086d761a012d8251841245e5c17afc
Fojan Babaali <fozhanmail@gmail.com>
2021-03-23 15:04:00 -0700

    Update Grid.js

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,16 +1,27 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
-
+import React, { Component } from "react";
+import styled from "styled-components";
+// -----------------------------------------------------*
 const StyleDiv = styled.div`
-color : red;
+  // regular CSS goes here :
+  //   Dynamic Grid
+  display: grid;
+  grid-template-columns: repeat(2, 70px);
+  grid-template-rows: repeat(2, 70px);
+  margin: 0 auto;
+  background-color: red;
+  border: 1px solid black;
+  margin-top : 20px;
 `;
 class Grid extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <StyleDiv>hi</StyleDiv>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <StyleDiv
+        numberOfcolumns={this.props.columns}
+        numberOfrows={this.props.rows}
+      ></StyleDiv>
+    );
+  }
 }
- 
+
 export default Grid;
