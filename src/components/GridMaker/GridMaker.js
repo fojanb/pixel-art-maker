f04298aadd2819b828bbf3ba70ed950506e8b818
Fojan Babaali <fozhanmail@gmail.com>
2021-03-23 00:29:40 -0700

    Update GridMaker.js

--- src/components/GridMaker/GridMaker.js
+++ src/components/GridMaker/GridMaker.js
@@ -12,7 +12,6 @@ class GridMaker extends Component {
     this.setState({ showGrid: !doesShow });
   };
   //-----------------Event Handlers (end)-------------*
-
   render() {
     // let grid = null;
     // if(this.state.showGrid){
@@ -21,17 +20,15 @@ class GridMaker extends Component {
     // }
     return (
       <div className="main">
-        <button type="reset" onClick={this.props.resetGrid}>
+        <button type="reset" onClick={this.props.resetGrid} id="resetButton">
           Reset
         </button>
         <div className="row-column">
-          <h3>Row</h3>
+          <h4>Row</h4>
           <button onClick={this.props.decrementRow}>-</button>
           <button onClick={this.props.incrementRow}>+</button>
           <span>{this.props.setRowBadge}</span>
-        </div>
-        <div className="row-column">
-          <h3>Column</h3>
+          <h4>Column</h4>
           <button onClick={this.props.decrementCol}>-</button>
           <button onClick={this.props.incrementCol}>+</button>
           <span>{this.props.setColBadge}</span>
