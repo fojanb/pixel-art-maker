d2e6341c838829fd4672a995515b3dccb904efed
Fojan Babaali <fozhanmail@gmail.com>
2021-03-25 00:34:09 -0700

    Update RowAndColumn.js

--- src/components/RowAndColumn/RowAndColumn.js
+++ src/components/RowAndColumn/RowAndColumn.js
@@ -22,11 +22,11 @@ class RowAndColumn extends Component {
           <h3>Row</h3>
           <button onClick={this.props.decrementRow}>-</button>
           <button onClick={this.props.incrementRow}>+</button>
-          <span>{this.props.setRowBadge}</span>
+          <span>{this.props.setRowBadge*10}</span>
           <h3>Column</h3>
           <button onClick={this.props.decrementCol}>-</button>
           <button onClick={this.props.incrementCol}>+</button>
-          <span>{this.props.setColBadge}</span>
+          <span>{this.props.setColBadge*10}</span>
         </div>
       </div>
     );
