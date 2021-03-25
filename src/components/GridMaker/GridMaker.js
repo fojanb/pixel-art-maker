7cafab37b631985c40008310f5dc67936c00bc09
Fojan Babaali <fozhanmail@gmail.com>
2021-03-25 00:25:22 -0700

    Update GridMaker.js

--- src/components/GridMaker/GridMaker.js
+++ src/components/GridMaker/GridMaker.js
@@ -7,8 +7,8 @@ const StyledGridContainer = styled.div`
   // regular CSS goes here :
   // *Dynamic grid via dynamic 'styled-component'*
   display: grid;
-  grid-template-columns: repeat(${(props) => props.numberOfColumns}, 20px);
-  grid-template-rows: repeat(${(props) => props.numberOfRows}, 20px);
+  grid-template-columns: repeat(${(props) => props.numberOfColumns}, 10px);
+  grid-template-rows: repeat(${(props) => props.numberOfRows}, 10px);
   margin: 0 auto;
   justify-content: center;
   align-item: center;
@@ -21,8 +21,8 @@ class GridMaker extends Component {
   render() {
     return (
       <StyledGridContainer
-        numberOfColumns={this.props.columns}
-        numberOfRows={this.props.rows}
+        numberOfColumns={this.props.columns*10}
+        numberOfRows={this.props.rows*10}
       >
         {this.props.children}
       </StyledGridContainer>
