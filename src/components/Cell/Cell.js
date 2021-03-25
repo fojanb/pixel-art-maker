6281f5376c6308bc430c2c7be974b013d0d0a427
Fojan Babaali <fozhanmail@gmail.com>
2021-03-25 00:25:19 -0700

    Update Cell.js

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -4,20 +4,18 @@ import "./Cell.css";
 
 const hoverStyle = keyframes`
 0% {
-  background: #C9F2EB linear-gradient(#C9F2EB 0%, #E1F9F5 49%, #D1DDDA 51%, #ADBBB8 100%);
-  border-color: #C9F2EB #B6D7D1 #ADBBB8 #B6D7D1;
-  box-shadow: 0 1px 3px rgba(191,110,38,0.5);
-  }
-  100% {
-    background: #ADBBB8 linear-gradient(#ADBBB8 0%, #D1DDDA 98%, #E1F9F5 99%, #C9F2EB 100%);
-    border-color: #ADBBB8 #B6D7D1 #C9F2EB #B6D7D1;
-    box-shadow: 0 -1px 1px rgba(191,110,38,0.5);
-  }`;
+    background-color: #caffbf;
+    border-color: #06d6a0;
+    }
+    100% {
+      background-color#06d6a0: #06d6a0;
+      border-color: #06d6a0;
+    }`;
 // Grid's cell - Styling
 const StyledDiv = styled.div`
   display: flex;
   border: 1px solid #06d6a0;
-//   background-color: #caffbf;
+  background-color: #06d6a0;
   animation-name: ${hoverStyle};
   animation-duration: 100ms;
   animation-fill-mode: backwards;
@@ -29,9 +27,19 @@ const StyledDiv = styled.div`
 `;
 class Cell extends Component {
   state = {};
+  stop = () => {
+    let cell = document.getElementById(this.props.id);
+    cell.style.backgroundColor = "#caffbf";
+  };
 
   render() {
-    return <StyledDiv className="cell" id={this.props.id}></StyledDiv>;
+    return (
+      <StyledDiv
+        className="cell"
+        onClick={this.stop}
+        id={this.props.id}
+      ></StyledDiv>
+    );
   }
 }
 
