37acf0015fd07b20d1ea4a6f0564f2a736173ec3
Fojan Babaali <fozhanmail@gmail.com>
2021-03-25 23:51:31 -0700

    Update Cell.js

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -18,17 +18,17 @@ const StyledDiv = styled.div`
   background-color: #06d6a0;
   animation-name: ${hoverStyle};
   animation-duration: 100ms;
-  animation-fill-mode: backwards;
+  animation-fill-mode: none;
   &:active,
   &:focus,
   &:hover {
-    animation-fill-mode: forwards;
+    animation-fill-mode: none;
   }
 `;
 class Cell extends Component {
   stop = () => {
     let cell = document.getElementById(this.props.id);
-    cell.style.backgroundColor = "#caffbf";
+    cell.style.backgroundColor = "#06d6a0";
   };
 
   render() {
